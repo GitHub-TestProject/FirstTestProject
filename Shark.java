6261adc4b3efba470fed472309084f7656f365e3
GitHub-TestProject <67316558+GitHub-TestProject@users.noreply.github.com>
2020-06-23 18:03:19 +0530

    Create Shark.java

---
+++ Shark.java
@@ -0,0 +1,12 @@
+public class Shark extends Fish{
+	
+	
+	 void swim() {		
+	     
+		System.out.println("I am swimming");
+		System.out.println("Sharks are large and grey ");
+		System.out.println("Sharks eat other fish  ");
+	
+	}
+  }
+	
