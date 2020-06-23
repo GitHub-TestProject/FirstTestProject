51a3d033f9e19c4ff244a9ba23da5aa4f42cbcd2
GitHub-TestProject <67316558+GitHub-TestProject@users.noreply.github.com>
2020-06-23 18:04:59 +0530

    Create ClownFish.java

---
+++ ClownFish.java
@@ -0,0 +1,11 @@
+public class ClownFish extends Fish{
+	
+		
+	 void swim() {		
+	     
+		System.out.println("I am swimming");
+		System.out.println("Clownfish are small and colourful (orange)");
+		System.out.println("Clownfish make jokes ");
+	
+	}
+	}
