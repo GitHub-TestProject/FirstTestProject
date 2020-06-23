94cb7af7f1d081e718c1087fcbe63e1f87030ca4
GitHub-TestProject <67316558+GitHub-TestProject@users.noreply.github.com>
2020-06-23 17:57:42 +0530

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,13 @@
+class Animal {
+	     
+    void walk(){
+    System.out.println("I am walking");
+    }
+    
+    void swim() {		
+	     
+		System.out.println("I am swimming");
+	
+	}
+    
+ }  
