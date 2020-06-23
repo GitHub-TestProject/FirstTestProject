3c5426b3d437a437de09c5bbd7c09f0db7e21c88
GitHub-TestProject <67316558+GitHub-TestProject@users.noreply.github.com>
2020-06-23 17:19:12 +0530

    Create Chicken.java

---
+++ Chicken.java
@@ -0,0 +1,18 @@
+public class Chicken extends Bird{
+	
+	
+	// overriding Birds sing method
+	 void sing() {		
+	     
+		System.out.println("Cluck, cluck");
+	
+	}
+	
+	// overriding Birds fly method
+	 void fly() {		
+	     
+		System.out.println("Chicken cannot fly");
+	
+	}
+
+}
